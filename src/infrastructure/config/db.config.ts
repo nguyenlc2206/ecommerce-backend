1be1510e0c30fab44b9cd20f5abb1e75410be7ba
nguyencaole <nguyenlc.smile@gmail.com>
2023-11-16 16:38:06 +0700

    update

--- src/infrastructure/config/db.config.ts
+++ src/infrastructure/config/db.config.ts
@@ -5,11 +5,9 @@ import mongoose from 'mongoose';
 import ENV from '@ecommerce-backend/src/main/config/env';
 import catchAsync from '@ecommerce-backend/src/shared/common/catchAsync';
 
-const DB_URL = ENV.databaseURL!.replace('<PASSWORD>', ENV.databasePassword!);
-
 /** @todo: connect to mongo database */
 const databaseConnection = catchAsync(async () => {
-    const connected = await mongoose.connect(DB_URL, {});
+    const connected = await mongoose.connect(ENV.databaseURL!, {});
     console.log(`Mongodb connected ${connected.connection.host}`);
     console.log('Connection database successful!');
 });
